d380c4b3d8a4a4b1aaf33c8a53ccbf4ae48aae62
GustavoLastra <gustavo.lastrac@gmail.com>
2017-08-30 15:38:19 +0200

    implemented: markerforplaces info listener, zoom after searchplace selection

--- js/AppViewModel.js
+++ js/AppViewModel.js
@@ -1,20 +1,99 @@
 /* ViewModel */
 var AppViewModel = function() {
-  var markersForPlaces = [];
   var self=this;
-  self.markers = [];
+  //var markers = [];
+  //var markersForPlaces = [];
+  self.placeTypes = ko.observableArray();
+  self.markersForPlaces = ko.observableArray();
+  self.markers = ko.observableArray();
   self.searchInput = ko.observable('');
+  self.place = ko.observable('');
   self.searchResult = ko.observableArray();
-  self.districts = ko.observableArray();
+  self.nameDistrict = ko.observable('Select a district');
+  self.nameService = ko.observable('Where do you want to go');
+
+
+  self.actualizeDistrict = ko.pureComputed({
+    read : function (){
+      return self.nameDistrict();
+    } ,
+    write: function (value) {
+      //console.log( value.title);
+      self.nameDistrict(isNaN(value.title) ? value.title : "No entiendo esto");
+
+      console.log("actualized district: " + self.nameDistrict());
+    },
+    owner: this
+  });
+  self.actualizeService = ko.pureComputed({
+    read : function (){
+      //console.log(self.nameService());
+      console.log("read service: " + self.nameService());
+      return self.nameService();
+    } ,
+    write: function (value) {
+      //console.log( value.title.toLowerCase());
+      self.nameService(isNaN(value.title) ? value.title : "No entiendo esto");
+      console.log("actualized service: " + self.nameService());
+      self.searchPlaces();
+    },
+    owner: this
+  });
+  /*self.checkPlace = ko.computed(function() {
+    console.log("1: " + self.markers()[0]);
+    console.log("2: " + self.markers().length);
+    console.log("3: " + self.nameDistrict());
+    for(var i=0; i<self.markers().length;i++){
+      console.log("4: " + self.makers());
+      if(self.nameDistrict()===self.makers[i].title){
+        location = self.markers().position;
+        console.log(MapView.map);
+        MapView.map.center = location;
+        MapView.map.zoom = 10;
+      } else {
+        radius = 1000;
+        location = {lat: 53.5511, lng: 9.9937};
+      }
+    }
+  });*/
+
+  // look for cafe "places"
+  self.searchPlaces = function() {
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(MapView.map);
+    var location;
+    var radius = 1000;
+
+    for(var i=0; i<Model.locations.length;i++){
+      if(self.nameDistrict()===Model.locations[i].title){
+        location = Model.locations[i].location;
+        //console.log(self.nameDistrict());
+        console.log( "Search Places: "+ location);
+        MapView.map.setCenter(location);
+        MapView.map.setZoom(15);
+      } else {
+        //radius = 1000;
+        //location = {lat: 53.5511, lng: 9.9937};
+      }
+    }
+    console.log("searchPlace: " + self.nameService().toLowerCase());
+    service.nearbySearch({
+      location: location,
+      radius: radius,
+      //type: ['cafe']
+      type: [self.nameService().toLowerCase()]
+    }, callback);
+}
+
+
 
   //Filter results("searchResult") based on "searchInput"
   self.filterResult = ko.computed(function() {
     var someInput = self.searchInput().toLowerCase();
     if (!someInput) {
         //if there is no filter, then return the whole list
-        for (i=0; i< markersForPlaces.length; i++) {
-          console.log(markersForPlaces.length);
-          markersForPlaces[i].setVisible(true);
+        for (i=0; i< self.markersForPlaces().length; i++) {
+          self.markersForPlaces()[i].setVisible(true);
         }
         return self.searchResult();
         console.log(self.searchResult());
@@ -24,13 +103,13 @@ var AppViewModel = function() {
         //if there is a filter then use arrayFilter to shorten the list
         return ko.utils.arrayFilter(self.searchResult(), function(item) {
           var string = item.name.toLowerCase();
-          for (i=0; i < markersForPlaces.length; i++) {
-            console.log(markersForPlaces[i]);
-            var str2 = markersForPlaces[i].title.toLowerCase();
+          for (i=0; i < self.markersForPlaces().length; i++) {
+            console.log(self.markersForPlaces()[i]);
+            var str2 = self.markersForPlaces()[i].title.toLowerCase();
             if(str2.search(someInput) >=0)
-              {markersForPlaces[i].setVisible(true);}
+              {self.markersForPlaces()[i].setVisible(true);}
             else
-              {markersForPlaces[i].setVisible(false);}
+              {self.markersForPlaces()[i].setVisible(false);}
             }
           if( string.search(someInput) >= 0 )
             {return true;}
@@ -39,16 +118,7 @@ var AppViewModel = function() {
         });
       }
   }, this);
-  // look for cafe "places"
-  self.searchCafe = function(){
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: Model.locations[1].location,
-      radius: 500,
-      type: ['cafe']
-    }, callback);
-  }
+
 
   function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -56,20 +126,21 @@ var AppViewModel = function() {
         self.searchResult.push(results[i]);
         createMarkersForPlaces(results[i],i);
       }
+      console.log(self.markersForPlaces());
     }
   }
   function createMarkersForPlaces(place,i) {
     var placeLoc = place.geometry.location;
-      console.log(place.name);
-      markersForPlaces.push(new google.maps.Marker({
-      map: map,
+      self.markersForPlaces.push(new google.maps.Marker({
+      map: MapView.map,
       animation: google.maps.Animation.DROP,
       position: place.geometry.location,
       title: place.name
     }));
 
-    markersForPlaces[i].addListener("click", function(){
+    self.markersForPlaces()[i].addListener("click", function(){
     populateInfoWindowForPlaces(this, MapView.largeInfowindow);
+    //MapView.bounds.extend(self.markersForPlaces()[i].position);
     });
   }
   function populateInfoWindowForPlaces(marker, infowindow){
@@ -77,7 +148,7 @@ var AppViewModel = function() {
       infowindow.marker = marker;
       console.log(marker);
       infowindow.setContent('<div>'+ marker.title +'</div>');
-      infowindow.open(map,marker);
+      infowindow.open(MapView.map,marker);
       infowindow.addListener("closeclick", function(){
         infowindow.setMarker(null);
       });
@@ -85,21 +156,26 @@ var AppViewModel = function() {
   }
 
 };
+AppViewModel.prototype.createPlaceTypes = function(list) {
+  for(var i=0; i<list.length;i++){
+    this.placeTypes.push(list[i]);
+  }
+};
 AppViewModel.prototype.createMarkers = function() {
   for(var i=0; i<Model.locations.length;i++){
+
     var position = Model.locations[i].location;
     var title = Model.locations[i].title;
     var marker = new google.maps.Marker({
       position: position,
       title: title,
-      map: map,
+      map: MapView.map,
       animation: google.maps.Animation.DROP,
       icon: MapView.defaultIcon,
       id: i
     });
 
     marker.addListener("click", function(){
-
       populateInfoWindow(this, MapView.largeInfowindow);
     });
     marker.addListener('mouseover', function() {
@@ -109,13 +185,13 @@ AppViewModel.prototype.createMarkers = function() {
       this.setIcon(MapView.defaultIcon);
     });
     MapView.bounds.extend(marker.position);
-    console.log(this);
     this.markers.push(marker);
+    //console.log(this.markers()[i].position);
     function populateInfoWindow(marker, infowindow){
       if(infowindow.marker != marker){
         infowindow.marker = marker;
         infowindow.setContent('<div>'+ marker.title +'</div>');
-        infowindow.open(map,marker);
+        infowindow.open(MapView.map,marker);
         infowindow.addListener("closeclick", function(){
           infowindow.setMarker(null);
         });
@@ -141,13 +217,11 @@ $(window).resize(function () {
     $('#map').css('height', (h - offsetTop));
 }).resize();
 
-$(function(){
+/*$(function(){
 
-   $(".dropdown-menu li a").click(function(){
-
-     $("#drop").text($(this).text());
-
+   $(".dropdown-menu .b li a").click(function(){
+     $(".btn:nth-child(4)").text($(this).text());
 
   });
 
-});
+});*/
--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 /*            Model          */
 var Model = {
    locations : [
-    {title: 'Hamburg, Germany', location: {lat:53.5511, lng: 9.9937}},
     {title: 'Hamburg-Mitte', location: {lat:53.5127, lng: 9.9875399}},
     {title: 'Eimsbüttel', location: {lat:53.574768, lng: 9.959453}},
     {title: 'Altona', location: {lat:53.5792, lng:9.8746}},
     {title: 'Hamburg-Nord', location: {lat:53.6118, lng:10.0073}},
     {title: 'Wandsbek', location: {lat: 53.6536, lng: 10.1070}},
     {title: 'Bergedorf', location: {lat:53.485984, lng:10.216255}},
-    {title: 'Harburg', location: {lat: 53.45, lng: 9.966667}}
+    {title: 'Hamburg', location: {lat: 53.45, lng: 9.966667}}
+  ],
+  places : [
+    {title: 'Cafe'},
+    {title: 'Restaurant'},
+    {title: 'Store'},
+    {title: 'Pharmacy'},
+    {title: 'Hospital'},
+    {title: 'Atm'},
+    {title: 'Museum'},
+    {title: 'Bank'},
+    {title: 'Gym'},
   ]
 }
 /*           Map View        */
@@ -19,12 +29,14 @@ var MapView = {
       this.bounds = new google.maps.LatLngBounds();
       this.defaultIcon = Octopus.control.makeMarkerIcon('0091ff');
       this.highlightedIcon = Octopus.control.makeMarkerIcon('FFFF24');
-      map = new google.maps.Map(document.getElementById('map'), {
+      this.map = new google.maps.Map(document.getElementById('map'), {
         center: {lat:  52.520008, lng: 13.404954},
         zoom: 8
-      })
+      });
+
       Octopus.control.createMarkers();
-      map.fitBounds(this.bounds);
+      Octopus.control.createPlaceTypes(Model.places);
+      this.map.fitBounds(this.bounds);
     }
 }
 
